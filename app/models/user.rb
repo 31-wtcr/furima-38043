f38d086d270deafad0a8d7ea1ec8c3ae21f94af4
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-09 18:22:04 +0900

    バリデーション修正、新規登録画面表示、ヘッダーボタンの条件分け

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname,              presence: true
   validates :last_name,             presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]\z/}
-  validates :firstname,             presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]\z/}
+  validates :first_name,             presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]\z/}
   validates :last_name_katakana,    presence: true, format:{ with: /\A[ァ-ヶ]\z/}
   validates :first_name_katakana,   presence: true, format:{ with: /\A[ァ-ヶ]\z/}
   validates :birthday,              presence: true
