3bb3420eeb7918e80261c5bf7a2393c6e3f21b20
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-09 21:56:31 +0900

    ユーザー新規登録機能 パスワード英数字混合未実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,              presence: true
-  validates :last_name,             presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]\z/}
-  validates :first_name,             presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]\z/}
-  validates :last_name_katakana,    presence: true, format:{ with: /\A[ァ-ヶ]\z/}
-  validates :first_name_katakana,   presence: true, format:{ with: /\A[ァ-ヶ]\z/}
+  validates :last_name,             presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :first_name,            presence: true, format:{ with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :last_name_katakana,    presence: true, format:{ with: /\A[ァ-ヶ]+\z/}
+  validates :first_name_katakana,   presence: true, format:{ with: /\A[ァ-ヶ]+\z/}
   validates :birthday,              presence: true
 
   
