882db4d4428b92e12c73bd4d76ba09a0a5c01344
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-12 16:39:23 +0900

    商品のバリデーションとアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  validates :item_name,        presence: true
+  validates :explanation,      presence: true
+  validates :category_id,      presence: true
+  validates :condition_id,     presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id,    presence: true
+  validates :days_to_ship_id,  presence: true
+  validates :price,            presence: true
+
+  belongs_to :user
+  has_one    :purchase
 end
