dc22f19a26833f674800bf9d55b9382bab6ecb5e
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-12 17:33:32 +0900

    active storage 導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def items_params
+    params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
