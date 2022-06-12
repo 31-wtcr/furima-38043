ba00a644065e10c91708556f6edfbc346a90fe6a
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-12 18:36:28 +0900

    出品するボタンで出品ページへ遷移
    ログイン時に出品ページへ遷移
    ログアウト時はログインページへ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,14 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+
+    @item = Item.new
+  end
+
   private
   def items_params
     params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
