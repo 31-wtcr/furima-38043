998ccd75dcfe1759e66dbec29fa9c5d078f4803e
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-13 23:35:52 +0900

    パラメータでimageを許可

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   private
 
   def items_params
-    params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
                                  :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
