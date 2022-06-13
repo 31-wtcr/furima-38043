ec5d4e4d8655942e9063deb5d4858e4ca1f8e008
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-13 14:17:46 +0900

    active_hashのアソシエーションとバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,22 @@ class Item < ApplicationRecord
 
   validates :item_name,        presence: true
   validates :explanation,      presence: true
-  validates :category_id,      presence: true
-  validates :condition_id,     presence: true
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id,    presence: true
-  validates :days_to_ship_id,  presence: true
+  validates :category_id,      presence: true, numericality: { other_than:1, message: "can't be blank" }
+  validates :condition_id,     presence: true, numericality: { other_than:1, message: "can't be blank" }
+  validates :shipping_cost_id, presence: true, numericality: { other_than:1, message: "can't be blank" }
+  validates :prefecture_id,    presence: true, numericality: { other_than:1, message: "can't be blank" }
+  validates :days_to_ship_id,  presence: true, numericality: { other_than:1, message: "can't be blank" }
   validates :price,            presence: true
 
   belongs_to :user
   has_one    :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
 end
