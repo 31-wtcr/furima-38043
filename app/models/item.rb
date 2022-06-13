6c78efb1cf56c8e80ae78ca7126303f40bcc26da
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-13 18:43:00 +0900

    商品価格のバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, presence: true, numericality: { other_than:1, message: "can't be blank" }
   validates :prefecture_id,    presence: true, numericality: { other_than:1, message: "can't be blank" }
   validates :days_to_ship_id,  presence: true, numericality: { other_than:1, message: "can't be blank" }
-  validates :price,            presence: true
+  validates :price,            presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   belongs_to :user
   has_one    :purchase
