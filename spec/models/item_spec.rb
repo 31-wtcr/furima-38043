65ca801089b31affa1d679d4c62c756f9d01160b
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-13 22:16:25 +0900

    商品のfactorybot作成、正常系単体テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+before do
+  @item = FactoryBot.build(:item)
+
+end
+  describe '商品の出品' do
+    context '商品の出品ができるとき' do
+      it '必要な情報を満たせば商品の出品ができる' do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
