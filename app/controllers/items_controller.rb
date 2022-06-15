8e9586cdf2f945581f5c3460ea6f17575803afdc
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-15 14:59:10 +0900

    商品編集機能 実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def index
     @items = Item.order(id: 'DESC')
   end
@@ -9,7 +15,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(items_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -18,12 +24,24 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+    redirect_to new_user_session_path and return unless user_signed_in?
+    redirect_to root_path and return if current_user != @item.user
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render 'edit'
+    end
   end
 
   private
 
-  def items_params
+  def item_params
     params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
                                  :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
