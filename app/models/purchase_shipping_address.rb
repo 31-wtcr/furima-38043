89d5fecd0c35aa89ce51048ee6ef0cb582bf8855
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-22 16:21:43 +0900

    都道府県を選択されていないときのバリデーション設定

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -6,7 +6,7 @@ class PurchaseShippingAddress
     validates :user_id
     validates :item_id
     validates :postal_code,                    format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture,                  numericality: { only_integer: true }
+    validates :prefecture,                  numericality: { other_than: 1, only_integer: true }
     validates :city
     validates :address
     validates :phone_number,                  format:{ with: /\A\d{10,11}\z/ }
